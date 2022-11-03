71ee227704f000c0f5e3691d058fba8ade2bb3a6
kaushal agarwal <agarwalkaushal2205@gmail.com>
2022-11-03 17:20:40 +0530

    Basic file structure

---
+++ src/main/java/com/wellsfargo/training/rockblack/RockblackApplication.java
@@ -0,0 +1,13 @@
+package com.wellsfargo.training.rockblack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockblackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockblackApplication.class, args);
+	}
+
+}
