b038bc09fd66183361f4ccb37bca9361729c44e7
Prathamesh <pbhutra26@gmail.com>
2022-11-09 13:16:18 +0530

    Changes in issues controller

--- src/main/java/com/wellsfargo/training/rockblack/controller/EmployeeIssueController.java
+++ src/main/java/com/wellsfargo/training/rockblack/controller/EmployeeIssueController.java
@@ -7,9 +7,7 @@ import com.wellsfargo.training.rockblack.service.ItemService;
 import com.wellsfargo.training.rockblack.service.LoanCardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -17,6 +15,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+@RestController
+@RequestMapping(value="/api")
 public class EmployeeIssueController {
     EmployeeIssueDetailsService issueService=new EmployeeIssueDetailsService();
     @Autowired
